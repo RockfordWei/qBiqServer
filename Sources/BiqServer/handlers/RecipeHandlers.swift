89dc1b0ca11ecbefc414f4997798fe7bdf873509
Rockford Wei <rocky@perfect.org>
2019-05-21 21:36:17 -0400

    Revert "fixing recipe decoding issue."
    This reverts commit 411a26669f7f64eeadd03c1f110df1649eb6a4ee.

--- Sources/BiqServer/handlers/RecipeHandlers.swift
+++ Sources/BiqServer/handlers/RecipeHandlers.swift
@@ -44,12 +44,10 @@ extension BiqRecipe: Comparable {
 
 	public init(from decoder: Decoder) throws {
 		let values = try decoder.container(keyedBy: CodingKeys.self)
-		_ = try values.decode(String.self, forKey: .uri)
-		let _title = try values.decode(String.self, forKey: .title)
-		let _author = try values.decode(String.self, forKey: .author)
-		let _description = try values.decode(String.self, forKey: .description)
-
-		self = BiqRecipe.init(title: _title, author: _author, description: _description)
+		uri = try values.decode(String.self, forKey: .uri)
+		title = try values.decode(String.self, forKey: .title)
+		author = try values.decode(String.self, forKey: .author)
+		description = try values.decode(String.self, forKey: .description)
 
 		// don't have to decode those non-native properties
 		do {
